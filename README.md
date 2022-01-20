1113e7ad1f65a342524b9596f0352300b964e9bb
charliegallop <c.gallop@protonmail.com>
2022-01-20 16:33:23 +0000

    First draft of report

--- README.md
+++ README.md
@@ -7,16 +7,15 @@ Data used for analysis:
   
 # ToDo
 
-- [ ] Consider bringing other varibales into data from UN or WHO website (things like fish production (reliance on ocean) etc) 
-- [ ] Check to see which method is the best fit for the data
+- [X] Consider bringing other varibales into data from UN or WHO website (things like fish production (reliance on ocean) etc) 
+- [X] Check to see which method is the best fit for the data
 - [X] Impute missing data using different estimator methods
 - [X] Build model
 
 	## Write up
 	
-- [ ] Add world maps for OHI and world happiness
-- [ ] Include scatter plots of variables used in model and happinessScore
-- [ ] Perform heirarchical modelling by region. Start with fixed effects model and follow the R tutorial from vle
-- [ ] 
+- [X] Add world maps for OHI and world happiness
+- [X] Include scatter plots of variables used in model and happinessScore
+- [X] Perform heirarchical modelling by region. Start with fixed effects model. 
 
 
--- rAnalysis/rAnalysis.R
+++ rAnalysis/rAnalysis.R
@@ -9,11 +9,17 @@ library(arm)
 library(readr)
 df <- read_csv("/home/charlie/Documents/Uni/Exeter - Data Science/MTHM601_Fundamentals_of_Applied_Data_Science/assignmentProjectv2/data/cleanData/finalTable.csv")
 
-lm1 <- lm(happinessScore ~ senseOfPlace, data = df) #+ cleanWater + tourismAndRec, data = df)
+lm1 <- lm(happinessScore ~ cleanWater, data = df) #+ cleanWater + tourismAndRec, data = df)
+lm2 <- lm(happinessScore ~ cleanWater * senseOfPlace, data = df)
+lm3 <- lm(happinessScore ~ senseOfPlace + cleanWater + tourismAndRec + artisnalOpp + biodiversity + foodProv + natProducts, data = df)
+lm4 <- lm(happinessScore ~ senseOfPlace + cleanWater + tourismAndRec + foodProv, data = df)
 lmer1 <- lmer(happinessScore ~ senseOfPlace + cleanWater + tourismAndRec + (1|region), data = df)
-lm2 <- lm(happinessScore ~ senseOfPlace + cleanWater + tourismAndRec + artisnalOpp + biodiversity + foodProv + natProducts, data = df)
 lmer2 <- lmer(happinessScore ~ senseOfPlace + cleanWater + tourismAndRec + artisnalOpp + biodiversity + foodProv + natProducts + (1|region), data = df)
+
+summary(lm1)
 summary(lm2)
+summary(lm3)
+
 
 lm1 %>% se.coef() %>% round(5)
 lmer1 %>% se.coef() %>% extract2(1) %>% round(5)
@@ -36,3 +42,25 @@ plot_model(lmer1, type = "re")
 
 par(mfrow = c(2, 2))
 plot(lm2)
+
+lm3 <- lm(happinessScore ~ senseOfPlace + cleanWater + tourismAndRec + artisnalOpp + biodiversity + foodProv + natProducts, data = df)
+
+ggplot(data = df, aes(x = index, y = happinessScore, col = region)) +
+  geom_point() +
+  stat_smooth(method = "lm", aes(col = region), alpha = 0.15) +
+  xlab("Ocean Health Index") +
+  ylab("Happiness Score") +
+  ggtitle("Relationship between happiness scores and the ocean health index")
+
+
+lmer1 <- lmer(happinessScore ~ senseOfPlace + cleanWater + tourismAndRec + artisnalOpp + biodiversity + foodProv + natProducts + (1|region), data = df)
+summary(lmer1)
+
+lmer2 <- lmer(happinessScore ~ senseOfPlace + cleanWater + tourismAndRec + artisnalOpp + biodiversity + foodProv + (1|region), data = df)
+summary(lmer2)
+
+lmer3 <- lmer(happinessScore ~ senseOfPlace + cleanWater + tourismAndRec + artisnalOpp + foodProv + (1|region), data = df)
+summary(lmer3)
+
+lmer4 <- lmer(happinessScore ~ senseOfPlace + cleanWater + artisnalOpp + foodProv + (1|region), data = df)
+summary(lmer4)
